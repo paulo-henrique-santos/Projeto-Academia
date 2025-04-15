ba3acd1254e397b74473d3228ba82c31af6c2d65
Paulo <paulosesisenai2024@gmail.com>
2025-04-15 15:56:39 -0300

    Segundo Commit

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 const baseUrl = "https://neo-gym-backend.vercel.app/academia"
 
 let cpfDoAluno = document.getElementById('cpf')
+async function buscarCpf() {
+    const cpf = cpfDoAluno.value
 
-async function pegarCpf() {
-    try {
-        const cpf = await fetch(baseUrl)
-
-        const cpfJson = await cpf.json()
-        cpfDoAluno = cpfJson.cpfDoAluno
+    const cpfBusca = {
+        cpf: cpf
     }
-    catch (error){
-        console.log('Erro ao chamar a API:'+error)
+    if (!cpf){
+        alert("Por favor, insira um CPF! 🙏")
+        return
+    }
+    try{
+        
+        const respostaHttp = await fetch(`${baseUrl}/consulta`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cpfBusca)
+        })
+        const cpfJson = await respostaHttp.json()
+
+        let cpfAluno = cpfJson.status
+        console.log(cpfAluno)
+
+        let resposta = ''
+        if (cpfAluno == 'true') {
+            resposta = "Liberado"
+        }
+        else {
+            resposta = "Bloqueado"
+        }
+
+        console.log(resposta)
+    } catch(erro) {
+        alert(`ERRO! Erro ao buscar o aluno ${erro.message}`)
     }
 }
-
-pegarCpf()
